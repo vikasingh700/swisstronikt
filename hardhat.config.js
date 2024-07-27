6ee5a6c6eda33192dcb8337aaca4ff913b167036
vikasingh700 <vikasingh700@gmail.com>
2024-07-27 12:27:05 +0000

    Initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const PRIVATE_KEY = "";
+
 module.exports = {
+  defaultNetwork: "swisstronik",
   solidity: "0.8.19",
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: [""], //Your private key starting with "0x"
+      accounts: [`0x` + `${PRIVATE_KEY}`],
     },
   },
 };
+
+
+
 
